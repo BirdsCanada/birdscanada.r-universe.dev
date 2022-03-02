398fa77d0d7c18ff2c4b1c72936b5e52df4ffc65
steffilazerte <steffi@steffi.ca>
2022-03-02 09:17:53 -0600

    Add motusData

--- universes.R
+++ universes.R
@@ -1,7 +1,8 @@
 df <- data.frame(
-  package = c("motus", "naturecounts"),
   url = c("https://github.com/MotusWTS/motus",
+          "https://github.com/MotusWTS/motusData",
           "https://github.com/birdscanada/naturecounts")
-)
+) |>
+  dplyr::mutate(package = stringr::str_extract(url, "[^\\/]+$"))
 
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
